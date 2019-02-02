c3a22a32e0862e405b8f400cfb9b9f1e988624a9
theshed1 <theshed1@users.noreply.github.com>
2019-02-02 16:33:13 +0000

    added pug files

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
 const express = require("express");
+const path = require("path");
 
+//init app
 const app = express();
 
+//load view engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+//home route
 app.get("/", function(req, res) {
-  res.send("hello world");
+  let articles = [
+    {
+      id: 1,
+      title: "Article one",
+      author: "Daniel Apps",
+      body: "This is article one"
+    },
+
+    {
+      id: 2,
+      title: "Article two",
+      author: "Vladimir Putin",
+      body: "This is article two"
+    },
+
+    {
+      id: 3,
+      title: "Article three",
+      author: "Daniel Apps",
+      body: "This is article three"
+    }
+  ];
+  res.render("index", {
+    title: "Articles",
+    articles: articles
+  });
 });
 
+// add route
+app.get("/articles/add", function(req, res) {
+  res.render("add_article", {
+    title: "Add Article"
+  });
+});
+
+//start server
 app.listen(3000, function() {
   console.log("sever started on port 3000");
 });
