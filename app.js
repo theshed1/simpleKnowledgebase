cfee05224a3a79b257e7ebfc86328c9e0d2da140
theshed1 <theshed1@users.noreply.github.com>
2019-02-04 12:35:42 +0000

    added server mumbo jumbo

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+mongoose.connect("mongodb://localhost/nodekb");
+let db = mongoose.connection;
+
+//check connection
+db.once("open", function() {
+  console.log("Connected to MongoDB");
+});
+
+//check for db errors
+db.on("error", function(err) {
+  console.log(err);
+});
 
 //init app
 const app = express();
+
+//bring in models
+let Article = require("./models/article");
 
 //load view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// body parser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extend: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 //home route
-app.get("/", function(req, res) {
+/*app.get("/", function(req, res) {
   let articles = [
     {
       id: 1,
@@ -36,12 +61,42 @@ app.get("/", function(req, res) {
     title: "Articles",
     articles: articles
   });
+});*/
+
+app.get("/", function(req, res) {
+  Article.find({}, function(err, articles) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", {
+        title: "Articles",
+        articles: articles
+      });
+    }
+  });
 });
 
 // add route
 app.get("/articles/add", function(req, res) {
   res.render("add_article", {
     title: "Add Article"
+  });
+});
+
+//add submit post route
+app.post("/articles/add", function(req, res) {
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save(function(err) {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      res.redirect("/");
+    }
   });
 });
 
